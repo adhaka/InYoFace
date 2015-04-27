e259ac2bdcfb73f81988314ca8a9bca09319d5cb
dario-wat <dario.vidas92@gmail.com>
2015-04-27 20:16:04 +0200

    Fixed VecAllFeatures

--- VecAllFeatures.m
+++ VecAllFeatures.m
@@ -1,9 +1,11 @@
 function fmat = VecAllFeatures(all_ftypes, W, H)
 
-%N = size(all_ftypes, 1);
-%fmat = zeros(N, W*H);
+N = size(all_ftypes, 1);
+fmat = zeros(N, W*H);
 
-fmat = rowfun(VecFeature, all_ftypes, 'W', W, 'H', H);
+for i = 1:N
+    fmat(i,:) = VecFeature(all_ftypes(i,:), W, H);
+end
 
 end
 
