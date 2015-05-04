53dd0ebab1fd70f067323accf75dd75f80f61213
adhaka <akash.uee@gmail.com>
2015-05-04 16:49:09 +0200

    bug fix in LoadImDataDir

--- LoadImDataDir.m
+++ LoadImDataDir.m
@@ -1,13 +1,12 @@
 function ii_ims = LoadImDataDir(dirname, varargin)
 
-face_fnames = dir('data/TrainingImages/FACES/*.bmp')
+face_fnames = dir([dirname, '*.bmp']);
 
-im_fname = ['data/TrainingImages/FACES/' , face_fnames(1).name]
+disp(dirname);
+disp(face_fnames);
+im_fname = [dirname , face_fnames(1).name];
 
 dirSize = length(face_fnames);
-
-[ii1, ii_im1] = LoadIm(im_fname);
-ii_ims = ii_im1(:);
 
 nVararg = length(varargin);
 disp(nVararg);
@@ -17,8 +16,12 @@ if nVararg > 0
     numImages = varargin{1};
 end
 
+disp(im_fname);
+[ii1, ii_im1] = LoadIm(im_fname);
+ii_ims = ii_im1(:);
+
 for i = 2:numImages
-    img = ['data/TrainingImages/FACES/', face_fnames(i).name]; 
+    img = [dirname, face_fnames(i).name]; 
     [ii, ii_im] = LoadIm(img);
     col_ii_iim = ii_im(:);
     ii_ims = [ii_ims col_ii_iim];
